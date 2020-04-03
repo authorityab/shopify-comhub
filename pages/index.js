73a86f0036957cd07975b49d5415f6a3476f8d4d
lounge <fredlunden@gmail.com>
2020-04-03 23:16:01 +0200

    styling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import { Layout, Page, TextStyle } from '@shopify/polaris';
+
 const Index = () => (
-  <div>
-    <p>LOOLOL</p>
-  </div>
+  <Page>
+   <Layout>
+     <p>Shopify-ComHub</p>
+     <TextStyle variation="positive">
+       1.0.0
+     </TextStyle>
+   </Layout>
+  </Page>
 );
 
 export default Index;
